defa2772e262f554f24ce51f13fd382b6857b3eb
MaximilianUAL2020 <maxi.zimmerer@gmail.com>
2021-04-19 13:11:37 +0100

    added basic content script functionality

--- assets/js/content.js
+++ assets/js/content.js
@@ -1,1 +1,34 @@
-console.log("Hello");
+// 1. Add image to DOM
+// 2. Get counter on load
+// 3. Get counter on stage change
+// 4. Set icon according to elapsed time
+
+let counter, active, limit, step;
+
+// get values from storage
+chrome.storage.local.get(["myCounter", "myState", "myLimit"], (result) => {
+  counter = result.myState ? result.myCounter : result.myLimit;
+  active = result.myState;
+  limit = result.myLimit;
+  step = limit / 5;
+  checkStep();
+});
+// listen to counter changes
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (namespace == "local") {
+    if (changes.myCounter) {
+      counter = changes.myCounter.newValue;
+    }
+    if (changes.myLimit) {
+      limit = changes.myLimit.newValue;
+      step = limit / 5;
+    }
+    checkStep();
+  }
+});
+// decrease healthbar
+function checkStep() {
+  if (counter % step) return;
+  let index = counter / step;
+  console.log(index);
+}
